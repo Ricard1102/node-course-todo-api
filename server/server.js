4998f0b8ea35446e2ea55de1cce6012cdd3f8331
Ricard Ribatallada Soriano <Ricard@MacBook-Pro-de-Ricard.local>
2018-05-11 12:24:20 +0100

    Add POST /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,74 +1,69 @@
-var mongoose = require('mongoose');
+var express = require('express');
+var bodyParser = require('body-parser');
 
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
 
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  },
-});
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
-var User = mongoose.model('User', {
-  Username: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }
+var app = express();
+
+//takes the bodyParser middleware. This is the middleware we need to send to express
+
+app.use(bodyParser.json());
+
+//resource creation. Body is created by body parser
+
+app.post('/todos', (req, res) => {
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc) => {
+    res.send(doc);
+  }, (e) => {
+    res.status(400).send(e);
+  });
 });
 
 
-var addUser = new User({
-  Username: 'Ricard',
-  email: 'ricard.ribatallada@gmail.com'
-});
-
-addUser.save().then((doc) => {
-  console.log('Saved user', doc);
-}, (e) => {
-  console.log('Unable to save user');
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 })
 
 
-
-// var newTodo = new Todo({
-//   text: 'Cook dinner'
+// var addUser = new User({
+//   Username: 'Ricard',
+//   email: 'ricard.ribatallada@gmail.com'
 // });
 
-var otherTodo = new Todo({
-  text: 'Edit this video',
-  completed: false,
-  completedAt: '01042018'
-});
-
-// newTodo.save().then((doc) => {
-//   console.log('Saved to do', doc);
+// addUser.save().then((doc) => {
+//   console.log('Saved user', doc);
 // }, (e) => {
-//   console.log('Unable to save todo');
+//   console.log('Unable to save user', e);
 // })
 
-// otherTodo.save().then((doc) => {
-//   console.log('Saved to do', doc);
-// }, (e) => {
-//   console.log('Unable to save todo');
-// })
+
+
+// // var newTodo = new Todo({
+// //   text: 'Cook dinner'
+// // });
+
+// var otherTodo = new Todo({
+//   text: 'Edit this video',
+//   completed: false,
+//   completedAt: '01042018'
+// });
+
+// // newTodo.save().then((doc) => {
+// //   console.log('Saved to do', doc);
+// // }, (e) => {
+// //   console.log('Unable to save todo');
+// // })
+
+// // otherTodo.save().then((doc) => {
+// //   console.log('Saved to do', doc);
+// // }, (e) => {
+// //   console.log('Unable to save todo');
+// // })
 
 
